f60077049eb9ea673038a3a83262c84040de1966
xuzhoukai <xuzhoukai2599@126.com>
2020-01-20 20:01:51 +0800

    first commit

---
+++ springmvc/src/main/java/com/omen/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.omen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/show")
+    public String show(){
+        return "user";
+    }
+}
